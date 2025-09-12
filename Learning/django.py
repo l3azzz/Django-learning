b6cc52e51d2201dd133173f64d26a9b05a926b4e
l3azzz <homecph1976@gmail.com>
2025-09-12 17:31:18 +0530

    main

---
+++ Learning/django.py
@@ -0,0 +1,30 @@
+# Intro to dJango
+
+
+# u know python is better than js 
+# To use python on backend, we can use django framework
+
+
+
+# go to current location to install the jango folder 
+# make a directory for the file with name --- mkdir django_1
+# then go to that directory we made --------- cd django_1
+# create a envirornment there so blah ------- python3 -m venv venv
+# now activate that thing to run     -------- venv\Scripts\activate.bat
+# create a src file in the folder   --------- mkdir src
+
+
+# inside src the entire code lies
+
+
+
+
+
+# install and run
+
+# first check which python it uses -- which python 
+# go to maing dir django_1         -- cd django_1
+# install djanog                   -- pip install django
+# now go to src folder --empty     -- cd src
+# now start the project ------------- django-admin startproject {current main folder name --django_1}
+# to run go to manage.py location  -- cd django_1 -- python manage.py runserver -thengo to the location using localhostblalh
