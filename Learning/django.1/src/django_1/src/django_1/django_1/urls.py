5e1ae8b501051981412569214836e32ac0448dbc
l3azzz <homecph1976@gmail.com>
2025-09-14 12:00:00 +0530

    suuuuper

--- Learning/django.1/src/django_1/src/django_1/django_1/urls.py
+++ Learning/django.1/src/django_1/src/django_1/django_1/urls.py
@@ -1,36 +1,8 @@
-"""
-URL configuration for django_1 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.http.response import HttpResponse
-
-#add two line break after importing
-
-
-
-def index(request): #needed for url construction err will happen -itneeds http response
-   # for the error import httpsresponse
-   return HttpResponse("<h1>Hello Fuck You</h1>")
-
-
-
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index) # if empty then call index function needed for url construction
+    path("", include("web.urls"))
 ]
